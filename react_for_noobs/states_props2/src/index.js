a8b66bf1e20cba81c469cb6f18a4b6263f332fb2
FBeaumont <flbeaumo@student.42.fr>
2019-03-11 14:30:19 +0100

    states and props ✅

---
+++ react_for_noobs/states_props2/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+	<App message="This is a test :D"/>,
+	document.getElementById('root')
+);
