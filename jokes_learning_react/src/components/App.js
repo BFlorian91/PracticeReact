8a36f1c8ea5e6695b717378673af4a869a2f9c88
FBeaumont <flbeaumo@student.42.fr>
2019-03-12 18:39:09 +0100

    pray for repo 🙏🏻 && learn basic 🐬  🚼

--- jokes_learning_react/src/components/App.js
+++ jokes_learning_react/src/components/App.js
@@ -1,37 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Joke from './Jokes'
 
-export default class App extends Component {
-	render() {
+/* import data */
+import jokesData from '../data/jokesData';
+
+export default function App() {
+	
+	const jokeComponents = jokesData.map((jokes) =>
+		<Joke key={jokes.id}
+					question={jokes.question}
+					punchLine={jokes.punchLine}
+		/>);
+
 		return (
 			<div className="boxOfQuestion">
-				<Joke punchLine="It’s hard to explain puns to kleptomaniacs because they always take things literally." />
-
-				<Joke 
-					question="What's the best thing about Switzerland?" 
-					punchLine="I don't know, but the flag is a big plus!"
-				/>
-
-				<Joke 
-					question="Did you hear about the mathematician who's afraid of negative numbers?" 
-					punchLine="He'll stop at nothing to avoid them!"
-				/>
-
-				<Joke 
-					question="Hear about the new restaurant called Karma?" 
-					punchLine="There’s no menu: You get what you deserve."
-				/>
-
-				<Joke 
-					question="Did you hear about the actor who fell through the floorboards?" 
-					punchLine="He was just going through a stage."
-				/>
-
-				<Joke 
-					question="Did you hear about the claustrophobic astronaut?" 
-					punchLine="He just needed a little space."
-				/>
+				{jokeComponents}
 			</div>
 		)
-	}
 }
