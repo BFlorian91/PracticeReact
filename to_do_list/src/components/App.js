6911ffd5e041a573919822e65b0fe090235e58e1
BFlorian91 <flbeaumo@student.42.fr>
2019-03-15 12:00:22 +0100

    learn basics 🐬 add states in TodoList ✅

--- to_do_list/src/components/App.js
+++ to_do_list/src/components/App.js
@@ -14,11 +14,25 @@ export default class App extends Component {
 		this.state = {
 			todos: todoData
 		}
+		this.handleChange = this.handleChange.bind(this)
 	}
 
+	handleChange(id) {
+		this.setState(prevState => {
+			const updatedTodos = prevState.todos.map(todo => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed
+				}
+				return todo
+			})
+			return {
+				todos: updatedTodos
+			}
+		})
+	}
 	render() {
-		const todoItems = this.state.todos.map((data) => <TodoItem key={data.id} item={data}/>);
-			
+		const todoItems = this.state.todos.map((data) => <TodoItem key={data.id} item={data} handleChange={this.handleChange}/>);
+
 		return (
 			<div className="todo-list">
 				<h1>TodoList</h1>
