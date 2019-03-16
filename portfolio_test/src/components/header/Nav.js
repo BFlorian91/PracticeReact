da0f2cacd19d7b25d0252bc1a883c695cb8e7cde
BFlorian91 <flbeaumo@student.42.fr>
2019-03-16 19:48:54 +0100

    practice -> fix navbar 🦄 ✅

--- portfolio_test/src/components/header/Nav.js
+++ portfolio_test/src/components/header/Nav.js
@@ -5,11 +5,11 @@ export default class Navbar extends Component {
 		return (
 			<div className="col s12">
 				<nav>
-					<div class="nav-wrapper blue lighten-2">
-						<a href="#!" class="brand-logo"><i class="material-icons">cloud</i>Logo</a>
-						<ul class="right hide-on-med-and-down">
-							<li><a><i class="material-icons">search</i></a></li>
-							<li><a href="#cards"><i class="material-icons">view_module</i></a></li>
+					<div className="nav-wrapper blue lighten-2">
+						<a href="#!" className="brand-logo"><i className="material-icons">cloud</i>Logo</a>
+						<ul className="right hide-on-med-and-down">
+							<li><a><i className="material-icons">search</i></a></li>
+							<li><a href="#cards"><i className="material-icons">view_module</i></a></li>
 						</ul>
 					</div>
 				</nav>
